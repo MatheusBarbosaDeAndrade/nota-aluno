77dd5a1f6a2a78793984b5ca5ae8798917767751
MatheusBarbosaDeAndrade <mathandrade658@gmail.com>
2024-09-20 20:10:53 -0300

    novo react

---
+++ components/title/style.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 20,
+    color: '#e2b24d'
+  }
+})
+
+export default styles
